480983ca886d25f828135073d08c6a517f58270a
Gerit Wagner <gerit.wagner@uni-bamberg.de>
2024-12-13 08:32:57 +0100

    course reset

---
+++ labot/course_reset.py
@@ -0,0 +1,20 @@
+import os
+import requests
+from pathlib import Path
+
+def main():
+    """Main function."""
+    
+    # replace all "- [x] " with "- [ ] " in the docs/* files
+    docs_dir = "docs"
+    for file in Path(docs_dir).glob("*"):
+        if not file.is_file():
+            continue
+        with open(file, 'r') as f:
+            contents = f.read()
+        contents = contents.replace("- [x] ", "- [ ] ")
+        with open(file, 'w') as f:
+            f.write(contents)
+
+if __name__ == "__main__":
+    main()
