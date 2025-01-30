b1134d5029839c1741ea6d699344fa675fd09b10
Gerit Wagner <gerit.wagner@uni-bamberg.de>
2025-01-30 08:56:16 +0100

    updates

--- labot/thesis.py
+++ labot/thesis.py
@@ -180,13 +180,13 @@ class Thesis:
                     file.write(line)
 
     def notify_for_inactive_students(self, github_repo: Github) -> None:
-        print('Notification for inactive students')
+        print("Notification for inactive students")
         if self.status != "registered":
             return
         # get last modified date of the file
         filename = Path("theses") / self.filename
         file = github_repo.get_contents(str(filename))
-        last_modified = file.last_modified
+        last_modified = datetime.strptime(file.last_modified, "%Y-%m-%d")
         days_since_last_modified = (datetime.now() - last_modified).days
         print(days_since_last_modified)
         if days_since_last_modified < 45:
--- labot/utils.py
+++ labot/utils.py
@@ -1,8 +1,8 @@
 #! /usr/bin/env python3
 """Labot utils."""
-
 from jinja2 import Environment
-from jinja2 import PackageLoader, Template
+from jinja2 import PackageLoader
+from jinja2 import Template
 
 
 def get_template(filename: str) -> Template:
