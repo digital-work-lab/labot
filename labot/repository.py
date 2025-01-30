87e22f50a989743cefb6d137e8ac07b2d63f6370
Gerit Wagner <gerit.wagner@uni-bamberg.de>
2025-01-30 08:51:26 +0100

    fix

--- labot/repository.py
+++ labot/repository.py
@@ -496,7 +496,7 @@ Please copy the [latest version](https://github.com/digital-work-lab/labot/blob/
             theses = labot.thesis.load_theses()
 
             for thesis in theses:
-                thesis.generate_gantt_chart_for_student(thesis)
+                thesis.generate_gantt_chart_for_student()
                 thesis.notify_for_inactive_students(self.github_repo)
 
             labot.thesis.generate_gantt(theses)
