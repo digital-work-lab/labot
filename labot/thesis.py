d6de02cbd74cf09ec131da15a799feac39e51214
Gerit Wagner <gerit.wagner@uni-bamberg.de>
2025-03-10 21:16:27 +0100

    create issue only if it does not exist

--- labot/thesis.py
+++ labot/thesis.py
@@ -196,8 +196,15 @@ class Thesis:
             return
         print(f"Notify {self.student} for inactivity")
         issue_title = f"Thesis Inactivity: {self.student}"
-        template = labot.utils.get_template("thesis_inactivity_issue.md.j2")
 
+        # Check if an issue with this title already exists
+        existing_issues = github_repo.get_issues(state="open")
+        for issue in existing_issues:
+            if issue.title == issue_title:
+                print("Issue already exists. Skipping creation.")
+                return
+
+        template = labot.utils.get_template("thesis_inactivity_issue.md.j2")
         github_repo.create_issue(
             title=issue_title,
             body=template.render(self.to_dict()),
