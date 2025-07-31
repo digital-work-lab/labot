9f2bca50d98e866273c27df5b1bdba8d9f4a6836
Gerit Wagner <gerit.wagner@uni-bamberg.de>
2025-07-31 14:12:22 +0200

    deactivate spellcheck

--- labot/repository.py
+++ labot/repository.py
@@ -12,7 +12,6 @@ from datetime import datetime
 from pathlib import Path
 
 import colrev.loader.load_utils
-import language_tool_python
 import requests
 import yaml
 from git import Repo
@@ -939,6 +938,7 @@ xychart-beta
         self._generate_mermaid_chart(theses)
 
     def run_spellcheck_and_manage_issue(self, wordlist_path=".wordlist.txt") -> None:
+        import language_tool_python
         issue_title = "Spellcheck Report"
 
         # Load custom wordlist
@@ -1076,8 +1076,8 @@ xychart-beta
 
         if self.REPO_NAME in ["work_hub"]:
             self._run_knowledge_repo_checks()
-        if self.REPO_NAME == "handbook":
-            self.run_spellcheck_and_manage_issue()
+        # if self.REPO_NAME == "handbook":
+            # self.run_spellcheck_and_manage_issue()
         if self.REPO_NAME == "theses":
             self._run_theses_checks()
 
