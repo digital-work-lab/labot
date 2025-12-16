db2df6c3100bb472ec441d067a9d1d9efa7e17ea
Gerit Wagner <gerit.wagner@uni-bamberg.de>
2025-12-16 10:46:21 +0100

    include cli:polish

--- labot/cli.py
+++ labot/cli.py
@@ -174,6 +174,17 @@ def local_cronjob(
 
     labot.local_cron.main()
 
+@main.command
+@click.pass_context
+def polish(
+    ctx: click.core.Context,
+) -> None:
+    "Polish files (using LLM)"
+    import labot.polish
+
+    labot.polish.main()
+
+
 
 @main.command()
 @click.pass_context
